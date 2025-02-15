13ccc59e95cfd96bed3b142baef5e73eb156c22f
Hiroaki Suzuki <rirazou05410@gmail.com>
2025-02-15 23:53:41 +0900

    コメントを修正

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Mermaidのコードブロックを、ハイライトではなくMermaidとして思慮できるようにクラスを変更
+  // Mermaidのコードブロックを、ハイライトではなくMermaidのダイアグラムとして表示できるようにクラスを変更
   const elems = document.querySelectorAll('.content code.language-mermaid');
   elems.forEach((elem) => {
     elem.classList.replace('language-mermaid', 'mermaid');
